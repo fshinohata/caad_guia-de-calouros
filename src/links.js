1bab341b20ccd1c71bc8d10e7ec396a20a275ed0
Laura Lopes <lauraslopes94@gmail.com>
2018-11-14 16:58:58 -0200

    pagina sobre com novo conteudo

--- src/links.js
+++ src/links.js
@@ -2,6 +2,7 @@ const links = {
 	semanaDosCalouros: "http://pet.inf.ufpr.br/calouros/index.php",
 	cardapioPolitecnico: "http://www.pra.ufpr.br/portal/ru/ru-centro-politecnico/",
 	calendarioCEPE: "http://caad.inf.ufpr.br/guia/files/CEPE4915.pdf",
+	historiaCAAD: "https://caad.inf.ufpr.br/index.php/historia/",
 	home: `/`,
 	primeirosPassos: `/primeiros-passos`,
 	sobre: `/sobre`,
