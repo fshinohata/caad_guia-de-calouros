d34d808c9b6ccf459ac43f179a340500d9e33d1c
Laura Lopes <lsl16@inf.ufpr.br>
2019-01-25 19:02:00 -0200

    mudança de icones

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -29,8 +29,13 @@ class About extends Component {
 							Esperamos que esse Guia tenha sido de alguma forma útil desde a escolha do curso até o primeiro ano como Calouro de BCC UFPR.
 							Todo feedback é bem vindo e pode ser enviado a <img src={Email} alt="Email do Centro Acadêmico Alexandre Direne" />
 						</p>
+            <p>
+              <div>
+                Icons made by <a href="https://www.flaticon.com/authors/monkik" title="monkik">monkik</a>, <a href="https://www.flaticon.com/authors/good-ware" title=" Good Ware">Good Ware</a>, <a href="https://www.flaticon.com/authors/smashicons" title=" Smashicons">Smashicons</a> and <a href="https://www.flaticon.com/authors/freepik" title=" Freepik">Freepik</a>   from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> are licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 		    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
+              </div>
+            </p>
 					</Text>
-				</Container>
+        </Container>
       		</StackUp>
         );
     }
